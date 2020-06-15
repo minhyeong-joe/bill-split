24267eea7b9c772f25add5afea57854858e450c3
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2020-06-15 16:30:39 -0700

    set up navigation

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
+// entry point of the app
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './src/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{
+                    title: 'BillSplit',
+                    headerStyle: {
+                        shadowOpacity: 0.75,
+                        shadowRadius: 5,
+                        shadowColor: '#111',
+                        shadowOffset: { height: 0, width: 0 }
+                    },
+                    headerTitleStyle: {
+                        textAlign: 'center',
+                        color: '#222',
+                        fontWeight: 'bold'
+                    }
+                }}>
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
